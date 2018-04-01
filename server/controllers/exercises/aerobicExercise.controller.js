629630269aac81edb317bc666e149b9e6bf5b532
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-01 22:33:04 +0200

    Added swagger spec of put aerobic exercise endpoint

--- server/controllers/exercises/aerobicExercise.controller.js
+++ server/controllers/exercises/aerobicExercise.controller.js
@@ -103,11 +103,6 @@ module.exports = function (app) {
      *         in: body
      *         required: true
      *         type: string
-     *       - name: idUser
-     *         description: Identificador único del usuario.
-     *         in: body
-     *         required: true
-     *         type: string
      *       - name: description
      *         description: Descripción del ejercicio.
      *         in: body
@@ -182,6 +177,11 @@ module.exports = function (app) {
      *         required: true
      *         type: string
      *         format: byte
+     *       - name: aerobicExercise
+     *         description: Identificador del ejercicio que se quiere listar.
+     *         in: path
+     *         required: true
+     *         type: string
      *     responses:
      *       200:
      *         description: Lista con todos los ejercicios.
@@ -208,6 +208,71 @@ module.exports = function (app) {
         });
     });
 
+    /**
+     * @swagger
+     * /aerobicExercises/{aerobicExercise}:
+     *   put:
+     *     tags:
+     *       - Aerobic exercises
+     *     summary: Edita un ejercicio aeróbico
+     *     description: Edita un ejercicio aeróbico existente únicamente si es personalizado.
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *       - name: aerobicExercise
+     *         description: Identificador del ejercicio que se quiere editar.
+     *         in: path
+     *         required: true
+     *         type: string
+     *       - name: name
+     *         description: Nombre del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: string
+     *       - name: category
+     *         description: Categoría del ejercicio aeróbico (p.ej., running, swimming).
+     *         in: body
+     *         required: true
+     *         type: string
+     *       - name: type
+     *         description: Tipo del ejercicio aeróbico (p.ej., crawl, butterfly, marathon, sprint).
+     *         in: body
+     *         required: true
+     *         type: string
+     *       - name: description
+     *         description: Descripción del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       400:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       401:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por no
+     *           tener un token correcto o tenerlo caducado.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
     router.put("/:aerobicExercise", function (req, res) {
 
         // Check if the exercise is custom
