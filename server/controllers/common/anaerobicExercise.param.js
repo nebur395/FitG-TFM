b9fcc06179224cfc101f13afb5fc307e2a87ce08
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-01 22:40:36 +0200

    Added anaerobic exercise param finder

---
+++ server/controllers/common/anaerobicExercise.param.js
@@ -0,0 +1,31 @@
+var models = require('../../models');
+var AnaerobicExercise = models.AnaerobicExercise;
+/**
+ * @name errorMessageHandler
+ * @desc Manage a http message from  a Mongoose error
+ * @param {Object} err - Mongoose error object
+ * @param {Object } res - Http response
+ */
+function anaerobicExerciseParam(req, res, next, id){
+
+    AnaerobicExercise.findOne({_id: id}, function (err, exercise) {
+        if (err) {
+            return errorMessageHandler(err, res);
+        }
+        if (!exercise) {
+            return res.status(404).send({
+                "message": "Anaerobic exercise does not exist."
+            });
+        }
+        if (exercise.idUser && (req.jwtPayload._id !== exercise.idUser.toString())) {
+            return res.status(401).send({
+                "message": "Invalid or non-existent token. Please, send a correct token."
+            });
+        }
+
+        req.anaerobicExercise = exercise;
+        return next();
+    });
+}
+
+exports.anaerobicExerciseParam = anaerobicExerciseParam;
