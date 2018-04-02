bd6ba011c800629e5ecc92816d4eee0f6a0e2317
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-02 16:41:29 +0200

    Added swagger spec of post aerobic marks

--- server/controllers/marks/aerobicMark.controller.js
+++ server/controllers/marks/aerobicMark.controller.js
@@ -76,7 +76,79 @@ module.exports = function (app) {
             });
     });
 
-
+    /**
+     * @swagger
+     * /aerobicExercises/{aerobicExercise}/aerobicMarks:
+     *   post:
+     *     tags:
+     *       - Aerobic exercises
+     *     summary: Crear una marca para un ejercicio aeróbico
+     *     description: Crea una nueva marca para un ejercicio aeróbico.
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *       - name: aerobicExercise
+     *         description: ID del ejercicio al que se quiere añadir la marca.
+     *         in: path
+     *         required: true
+     *         type: string
+     *       - name: distance
+     *         description: Descripción del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: number
+     *       - name: time
+     *         description: Descripción del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: number
+     *       - name: intensity
+     *         description: Descripción del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: number
+     *       - name: heartRate
+     *         description: Descripción del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: number
+     *       - name: comment
+     *         description: Descripción del ejercicio.
+     *         in: body
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Un ejercicio aeróbico con toda su información.
+     *         schema:
+     *           type: object
+     *           properties:
+     *              exercise:
+     *                $ref: '#/definitions/AerobicExercise'
+     *       400:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       401:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por no
+     *           tener un token correcto o tenerlo caducado.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
     router.post("/:aerobicExercise/aerobicMarks", function (req, res) {
 
         var newMark = new AerobicMark();
