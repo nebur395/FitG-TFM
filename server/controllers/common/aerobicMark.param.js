170e3597439bf2cd643f256a0b980ff6d5185262
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-03 01:26:24 +0200

    Added aerobic mark route parameter function

---
+++ server/controllers/common/aerobicMark.param.js
@@ -0,0 +1,33 @@
+var models = require('../../models');
+var AerobicMark = models.AerobicMark;
+var errorMessageHandler = require('./errorMessageHandler').errorMessageHandler;
+
+/**
+ * @name errorMessageHandler
+ * @desc Manage a http message from  a Mongoose error
+ * @param {Object} err - Mongoose error object
+ * @param {Object } res - Http response
+ */
+function aerobicMarkParam(req, res, next, id){
+
+    AerobicMark.findOne({_id: id}, function (err, mark) {
+        if (err) {
+            return errorMessageHandler(err, res);
+        }
+        if (!mark) {
+            return res.status(404).send({
+                "message": "Aerobic mark does not exist."
+            });
+        }
+        if (mark.idUser && (req.jwtPayload._id !== mark.idUser.toString())) {
+            return res.status(401).send({
+                "message": "Invalid or non-existent token. Please, send a correct token."
+            });
+        }
+
+        req.aerobicMark = mark;
+        return next();
+    });
+}
+
+exports.aerobicMarkParam = aerobicMarkParam;
