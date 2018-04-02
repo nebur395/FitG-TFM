cc4ec3bf967a2c6682adefbb6729150e223ed4be
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-02 15:51:35 +0200

    Added swagger spec of get aerobic marks

--- server/controllers/marks/aerobicMark.controller.js
+++ server/controllers/marks/aerobicMark.controller.js
@@ -13,6 +13,52 @@ module.exports = function (app) {
         aerobicExerciseParam(req, res, next, id)
     });
 
+    /**
+     * @swagger
+     * /aerobicExercises/{aerobicExercise}/aerobicMarks:
+     *   post:
+     *     tags:
+     *       - Aerobic marks
+     *     summary: Listar las marcas de un ejercicio aeróbico.
+     *     description: Lista todas las marcas de un ejercicio aeróbico.
+     *     consumes:
+     *       - application/json
+     *       - charset=utf-8
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: Authorization
+     *         description: |
+     *           JWT estándar: `Authorization: Bearer + JWT`.
+     *         in: header
+     *         required: true
+     *         type: string
+     *         format: byte
+     *       - name: aerobicExercise
+     *         description: Identificador del ejercicio que tiene las marcas.
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Lista con todas las marcas.
+     *         schema:
+     *           type: object
+     *           properties:
+     *              marks:
+     *               type: array
+     *               items:
+     *                $ref: '#/definitions/AerobicMark'
+     *       401:
+     *         description: Mensaje de feedback para el usuario. Normalmente causado por no
+     *           tener un token correcto o tenerlo caducado.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     *       500:
+     *         description: Mensaje de feedback para el usuario.
+     *         schema:
+     *           $ref: '#/definitions/FeedbackMessage'
+     */
     router.get("/:aerobicExercise/aerobicMarks", function (req, res) {
         AerobicMark.find({$and: [
                 {idUser: req.jwtPayload._id},
