f3911101499a0e9d9273a3f8c6d3d96f89622d32
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-06 15:44:32 +0200

    Added post body analisis

--- server/controllers/bodyAnalisis/bodyAnalisis.controller.js
+++ server/controllers/bodyAnalisis/bodyAnalisis.controller.js
@@ -61,5 +61,52 @@ module.exports = function (app) {
             });
     });
 
+
+    router.post("/", function (req, res) {
+        var startToday = new Date();
+        startToday.setHours(0,0,0,0);
+
+        var endToday = new Date();
+        endToday.setHours(23,59,59,999);
+
+        // Find if there is any body analisis created in the same day
+        BodyAnalisis.find({creationDate: {$gte: startToday, $lt: endToday}}, function (err, analisis) {
+            if (err) {
+                return errorMessageHandler(err, res);
+            } else if (analisis) {
+                return res.status(400).send({
+                    "message": "Body analisis already created today."
+                });
+            } else {
+                var newAnalisis = new BodyAnalisis();
+
+                // Add the new attributes to the body analisis object
+                newAnalisis.weight = req.body.weight;
+                newAnalisis.bmi = req.body.bmi;
+                newAnalisis.metabolicAge = req.body.metabolicAge;
+                newAnalisis.basalMetabolism = req.body.basalMetabolism;
+                newAnalisis.bodyFat = req.body.bodyFat;
+                newAnalisis.muscleMass = req.body.muscleMass;
+                newAnalisis.boneMass = req.body.boneMass;
+                newAnalisis.bodyFluids = req.body.bodyFluids;
+                newAnalisis.visceralAdiposity = req.body.visceralAdiposity;
+                newAnalisis.dailyCaloricIntake = req.body.dailyCaloricIntake;
+                newAnalisis.idUser = req.jwtPayload._id;
+
+                newAnalisis.save(function (err, exercise) {
+                    if (err) {
+                        return errorMessageHandler(err, res);
+                    } else {
+                        exercise = exercise.toJSON();
+                        delete exercise.__v;
+                        return res.status(200).send({
+                            "analisis": analisis
+                        });
+                    }
+                });
+            }
+        });
+    });
+
     return router;
 };
