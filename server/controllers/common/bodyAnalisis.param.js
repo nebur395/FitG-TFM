7051c7a2f272714375998a70f48829e9d3c7e1b0
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-06 12:15:30 +0200

    Added body analisis parameter request

---
+++ server/controllers/common/bodyAnalisis.param.js
@@ -0,0 +1,33 @@
+var models = require('../../models');
+var BodyAnalisis = models.BodyAnalisis;
+var errorMessageHandler = require('./errorMessageHandler').errorMessageHandler;
+
+/**
+ * @name errorMessageHandler
+ * @desc Manage a http message from  a Mongoose error
+ * @param {Object} err - Mongoose error object
+ * @param {Object } res - Http response
+ */
+function bodyAnalisisParam(req, res, next, id){
+
+    BodyAnalisis.findOne({_id: id}, function (err, analisis) {
+        if (err) {
+            return errorMessageHandler(err, res);
+        }
+        if (!analisis) {
+            return res.status(404).send({
+                "message": "Body analisis does not exist."
+            });
+        }
+        if (analisis.idUser && (req.jwtPayload._id !== analisis.idUser.toString())) {
+            return res.status(401).send({
+                "message": "Invalid or non-existent token. Please, send a correct token."
+            });
+        }
+
+        req.bodyAnalisis = analisis;
+        return next();
+    });
+}
+
+exports.bodyAnalisisParam = bodyAnalisisParam;
