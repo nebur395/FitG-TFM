5a9fa9c94accf5ce592746e723e0f8b9d2889e86
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-06 23:49:43 +0200

    Added test suite for delete body analysis

--- test/mocha/bodyAnalisis/deleteBodyAnalisis.js
+++ test/mocha/bodyAnalisis/deleteBodyAnalisis.js
@@ -4,7 +4,7 @@ var chai = require('chai'),
     server = require('../../../server'),
     userCommon = require('../../common/userCommon'),
     createUserToken = require('../../common/jwtCreator').createUserToken,
-    anaerobicExerciseCommon = require('../../common/anaerobicExerciseCommon'),
+    bodyAnalisisCommon = require('../../common/bodyAnalisisCommon'),
     feedbackMessageCommon = require('../../common/feedbackMessageCommon');
 
 chai.use(chaiHttp);
@@ -19,14 +19,13 @@ describe('AnaerobicExercise', function () {
         email2 = "Testing2@email.com",
         password = "Testing";
 
-    var exercisesId = [],
+    var analisisId = [],
         idUsers = [];
 
-    var notExists = "Anaerobic exercise does not exist.",
-        isNotCustom = "Predefined exercises can not be deleted.",
+    var notExists = "Body analisis does not exist.",
         invalidToken = "Invalid or non-existent token. Please, send a correct token.",
         invalidID = "Cast to ObjectId failed",
-        successMessage = "Anaerobic exercise deleted successfully.";
+        successMessage = "Body analisis deleted successfully.";
 
     /*
      * It creates a new entities before the test suite starts executing.
@@ -37,26 +36,23 @@ describe('AnaerobicExercise', function () {
             idUsers.push(id);
             userCommon.createUser(username, email2, password, function(id) {
                 idUsers.push(id);
-                anaerobicExerciseCommon.createCustomAnaerobicExercise(idUsers[0], function (id) {
-                    exercisesId.push(id);
-                    anaerobicExerciseCommon.createPredefinedAnaerobicExercise(idUsers[0], function(id){
-                        exercisesId.push(id);
-                        done();
-                    });
+                bodyAnalisisCommon.createBodyAnalisis(idUsers[0], function (id) {
+                    analisisId.push(id);
+                    done();
                 });
             });
         });
     });
 
     /**
-     * Tests for anaerobicExercise functionality.
+     * Tests for delete body analisis functionality.
      */
-    describe('#deleteAnaerobicExercise()', function () {
+    describe('#deleteBodyAnalisis()', function () {
 
-        it('should return an error message since the user isn\'t the owner of the exercise', function (done) {
+        it('should return an error message since the user isn\'t the owner of the analisis', function (done) {
 
             chai.request(server)
-                .delete('/anaerobicExercises/' + exercisesId[0])
+                .delete('/bodyAnalisis/' + analisisId[0])
                 .set('Authorization','Bearer ' + createUserToken(idUsers[1], email, username))
                 .end(function (err, result) {
 
@@ -66,10 +62,10 @@ describe('AnaerobicExercise', function () {
                 });
         });
 
-        it('should delete an existing anaerobic exercise', function (done) {
+        it('should delete an existing body analysis', function (done) {
 
             chai.request(server)
-                .delete('/anaerobicExercises/' + exercisesId[0])
+                .delete('/bodyAnalisis/' + analisisId[0])
                 .set('Authorization','Bearer ' + createUserToken(idUsers[0], email, username))
                 .end(function (err, result) {
 
@@ -78,10 +74,10 @@ describe('AnaerobicExercise', function () {
                 });
         });
 
-        it('should return an error message since the exercise doesn\'t exists', function (done) {
+        it('should return an error message since the analysis doesn\'t exists', function (done) {
 
             chai.request(server)
-                .delete('/anaerobicExercises/' + exercisesId[0])
+                .delete('/bodyAnalisis/' + analisisId[0])
                 .set('Authorization','Bearer ' + createUserToken(idUsers[0], email, username))
                 .end(function (err, result) {
 
@@ -91,10 +87,10 @@ describe('AnaerobicExercise', function () {
                 });
         });
 
-        it('should return an error message since the exercise doesn\'t exists', function (done) {
+        it('should return an error message since the analysis doesn\'t exists', function (done) {
 
             chai.request(server)
-                .delete('/anaerobicExercises/' + '123')
+                .delete('/bodyAnalisis/' + '123')
                 .set('Authorization','Bearer ' + createUserToken(idUsers[0], email, username))
                 .end(function (err, result) {
 
@@ -104,27 +100,14 @@ describe('AnaerobicExercise', function () {
                 });
         });
 
-        it('should return an error message since is not a custom exercise', function (done) {
-
-            chai.request(server)
-                .delete('/anaerobicExercises/' + exercisesId[1])
-                .set('Authorization','Bearer ' + createUserToken(idUsers[0], email, username))
-                .end(function (err, result) {
-
-                    feedbackMessageCommon.checkMessageCode(result, 400, isNotCustom);
-
-                    done();
-                });
-        });
-
         /*
-         * Removes the entities created during the signIn tests.
+         * Removes the exercises created during the signIn tests.
          */
         after(function (done) {
 
             userCommon.deleteUserById(idUsers[0], function () {
                 userCommon.deleteUserById(idUsers[1], function () {
-                    anaerobicExerciseCommon.deleteAnaerobicExerciseById(exercisesId, done);
+                    bodyAnalisisCommon.deleteBodyAnalisisById(analisisId, done);
                 });
             });
         });
