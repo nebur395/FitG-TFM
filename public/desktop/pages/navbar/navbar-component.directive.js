e839f67bfd441ddc16d2fcd8ba5c07b1d4bb1c91
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-07 19:11:36 +0200

    Added navbar controller client file

---
+++ public/desktop/pages/navbar/navbar-component.directive.js
@@ -0,0 +1,19 @@
+angular.module('fitGApp')
+
+    .controller('navbarCtrl', ['$scope', 'authService', function ($scope, authService) {
+
+        $scope.home = "";
+        $scope.logged = false;
+
+        // Watches to control if the user is authenticated
+        $scope.$watch(function () {
+            return authService.isAuthenticated();
+        }, function () {
+            $scope.logged = authService.isAuthenticated();
+            $scope.home = $scope.logged ? "exercises" : "starter";
+        });
+
+        $scope.logout = function () {
+            authService.logout();
+        }
+    }]);
