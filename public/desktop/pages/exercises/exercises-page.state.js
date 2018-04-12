80707525e19abcdc5d47464d063a02076a571936
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-13 01:38:06 +0200

    Desktop side: Added exercise type button filter

--- public/desktop/pages/exercises/exercises-page.state.js
+++ public/desktop/pages/exercises/exercises-page.state.js
@@ -3,14 +3,20 @@ angular.module('fitGApp')
     .controller('exercisesCtrl', ['$scope', 'aerobicExerciseService', 'notificationService',
         function ($scope, aerobicExerciseService, notificationService) {
 
+            $scope.exerciseType = "Aerobic";
+
+            $scope.changeExerciseType = function (type) {
+                $scope.exerciseType = type;
+            };
+
             // inputs visual variables
-            $scope.exercisesList = [];
+            $scope.aerobicExerciseList = [];
 
             // send the login form to the auth service
             $scope.getAerobicExerises = function () {
                 aerobicExerciseService.getExercises()
                     .then(exercises => {
-                        $scope.exercisesList = exercises;
+                        $scope.aerobicExerciseList = exercises;
                     })
                     .catch(error => {
                         notificationService.showError('&#10008', error.message);
