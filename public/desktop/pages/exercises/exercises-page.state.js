a8ebeeae264de8e1e4a29638b80600ffc4dac106
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-13 12:56:41 +0200

    Desktop side: Improved filter styles

--- public/desktop/pages/exercises/exercises-page.state.js
+++ public/desktop/pages/exercises/exercises-page.state.js
@@ -3,14 +3,14 @@ angular.module('fitGApp')
     .controller('exercisesCtrl', ['$scope', 'aerobicExerciseService', 'notificationService',
         function ($scope, aerobicExerciseService, notificationService) {
 
+            // inputs visual variables
+            $scope.aerobicExerciseList = [];
+            $scope.anaerobicExerciseList = [];
             $scope.exerciseType = "Aerobic";
 
             $scope.changeExerciseType = function (type) {
                 $scope.exerciseType = type;
             };
-
-            // inputs visual variables
-            $scope.aerobicExerciseList = [];
 
             // send the login form to the auth service
             $scope.getAerobicExerises = function () {
