58d65551d305df029c79b85764eec075f781b0c8
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-15 21:53:25 +0200

    Desktop side: Modified starter style and deleted navbar from start

--- public/desktop/pages/navbar/navbar-component.directive.js
+++ public/desktop/pages/navbar/navbar-component.directive.js
@@ -2,17 +2,6 @@ angular.module('fitGApp')
 
     .controller('navbarCtrl', ['$scope', 'authService', function ($scope, authService) {
 
-        $scope.home = "";
-        $scope.logged = false;
-
-        // Watches to control if the user is authenticated
-        $scope.$watch(
-            () => authService.isAuthenticated(),
-            function () {
-                $scope.logged = authService.isAuthenticated();
-                $scope.home = $scope.logged ? "exercises" : "starter";
-            });
-
         $scope.logout = function () {
             authService.logout();
         }
