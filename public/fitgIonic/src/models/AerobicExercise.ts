c265c06f5b52231c125be10c780f723d67278c43
Ruben Moreno Jimeno <nebur395@hotmail.com>
2018-04-17 20:18:41 +0200

    Ionic side: Added exercise models

---
+++ public/fitgIonic/src/models/AerobicExercise.ts
@@ -0,0 +1,9 @@
+export class AerobicExercise {
+  _id: string;
+  name: string;
+  category: string;
+  type: string;
+  custom: boolean;
+  idUser: string;
+  description: string;
+}
---
+++ public/fitgIonic/src/models/AnaerobicExercise.ts
@@ -0,0 +1,9 @@
+export class AnaerobicExercise {
+  _id: string;
+  name: string;
+  category: string;
+  type: string;
+  custom: boolean;
+  idUser: string;
+  description: string;
+}
